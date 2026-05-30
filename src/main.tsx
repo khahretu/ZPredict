21189b32514554d40fda45f4db566aeb7cf6f169
khahretu <Khahretu@internet.ru>
2026-05-30 13:25:18 +0530

    Add Web3Wrapper component to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-import Web3Wrapper from './web3/Web3Wrapper';
+// Is wrapper ko hum blank rakhenge taaki website crash na ho
+const Web3Wrapper = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
