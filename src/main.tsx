a562c4a2cdc545444666cb25d37d81d7890209e6
khahretu <Khahretu@internet.ru>
2026-05-30 13:38:16 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Web3Wrapper ko bypass karo taaki external scripts chal sakein
+// Direct App render karo bina kisi wrapper ke
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <App />
